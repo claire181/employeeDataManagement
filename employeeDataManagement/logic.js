c374ddafe187dc1d87e07a735d32a28e35e0825c
Claire Allen <claireallen@Claires-MacBook-Pro.local>
2017-05-20 11:10:05 -0400

    updated with firebase

---
+++ employeeDataManagement/logic.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBQpZHUfUeEcePB1KNnnp3SaoNpIyo-aas",
+    authDomain: "employeedatamanagement-37a5e.firebaseapp.com",
+    databaseURL: "https://employeedatamanagement-37a5e.firebaseio.com",
+    projectId: "employeedatamanagement-37a5e",
+    storageBucket: "employeedatamanagement-37a5e.appspot.com",
+    messagingSenderId: "1035127999662"
+  };
+  firebase.initializeApp(config);
+</script>
